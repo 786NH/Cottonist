de793ea9b3e1c26a0764a6503e342857e4a10c97
786NH <navedhasanmbd12@gmail.com>
2025-02-14 23:54:40 +0530

    Second

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listview/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,94 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ListViewPage(),
+      home:LoginScreen(),
     );
   }
 }
 
-class ListViewPage extends StatefulWidget {
-  const ListViewPage({
-    super.key,
-  });
-
-  @override
-  State<ListViewPage> createState() => _MyListState();
-}
-
-TextEditingController textEditingController = TextEditingController();
-
-class _MyListState extends State<ListViewPage> {
-  List<String> fruit = ["Apple", "Banana", "Oranges"];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 251, 247, 247),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 87, 165, 230),
-        title: const Text("List View Builder Demo"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                addList(context);
-              },
-              icon: const Icon(Icons.add))
-        ],
-      ),
-      body: ListView.builder(
-          itemCount: fruit.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                title: Text(fruit[index]),
-                trailing: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      fruit.removeAt(index);
-                    });
-                  },
-                  icon: const Icon(Icons.delete),
-                ),
-              ),
-            );
-          }),
-    );
-  }
-
-  void addList(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Add a Fruit"),
-          content: TextField(
-            controller: textEditingController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "Enter fruit name",
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context); // Close dialog
-              },
-              child: const Text("Cancel"),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  if (textEditingController.text.trim().isNotEmpty) {
-                    fruit.add(textEditingController.text.trim());
-                    textEditingController.clear();
-                  }
-                });
-                Navigator.pop(context); // Close dialog
-              },
-              child: const Text("Add"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
